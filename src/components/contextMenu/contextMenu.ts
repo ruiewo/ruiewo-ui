69df1187e43567fb9d680dbd5e7af716c464a6f0
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-07-25 17:02:12 +0900

    add ContextMenu class

---
+++ src/components/contextMenu/contextMenu.ts
@@ -0,0 +1,115 @@
+import { calcPositionFromPoint, MenuItem, PositionOption } from '../helper';
+import { MenuPanel } from '../menuPanel/menuPanel';
+
+const css = `
+`;
+const html = `
+`;
+
+const template = `<style>${css}</style>${html}`;
+
+let currentMenu: ContextMenu | null = null;
+
+export type ContextMenuOption = {
+    onSelect: (value: string) => void;
+};
+
+const defaultOption = {
+    onSelect: () => {},
+};
+
+export class ContextMenu extends HTMLElement {
+    private self: ContextMenu;
+    private root: ShadowRoot;
+    private host: HTMLElement;
+    private menu: MenuPanel;
+    private items: MenuItem[] = [];
+    private option: ContextMenuOption;
+
+    private position: PositionOption = {
+        vertical: 'auto',
+        horizontal: 'auto',
+    };
+
+    constructor(element: HTMLElement, items: MenuItem[], userOption?: Partial<ContextMenuOption>) {
+        super();
+        this.self = this;
+        this.root = this.attachShadow({ mode: 'open' });
+        this.root.innerHTML = template;
+
+        this.host = this.root.host as HTMLElement;
+
+        this.option = Object.assign({}, defaultOption, userOption);
+        this.items = items;
+
+        this.menu = new MenuPanel();
+        this.root.appendChild(this.menu);
+
+        this.menu.onClick = item => {
+            this.close();
+        };
+
+        element.addEventListener('contextmenu', e => {
+            e.preventDefault();
+            this.show(e);
+        });
+
+        // this.input.onkeydown = e => {
+        //     const keyCode = e.code;
+
+        //     switch (keyCode) {
+        //         case 'Enter':
+        //             this.menu.select();
+        //             break;
+        //         case 'ArrowDown':
+        //             this.menu.selectNext();
+        //             break;
+        //         case 'ArrowUp':
+        //             this.menu.selectPrev();
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // };
+    }
+
+    show(e: MouseEvent) {
+        closeMenuPanel();
+
+        this.menu.show(this.items);
+        currentMenu = this.self;
+
+        console.log('show rui-context');
+
+        this.updatePosition(e);
+    }
+
+    updatePosition(e: MouseEvent) {
+        const { left, top } = calcPositionFromPoint(e, this.menu, this.position);
+        this.menu.updatePosition({ left, top });
+    }
+
+    close() {
+        this.menu.close();
+        currentMenu = null;
+    }
+}
+
+function closeMenuPanel() {
+    if (currentMenu != null) {
+        currentMenu.close();
+    }
+}
+
+function initialize() {
+    customElements.define('ruiewo-contextmenu', ContextMenu);
+    document.addEventListener('mouseup', function (e) {
+        if ((e.target as HTMLElement).closest('ruiewo-contextmenu')) {
+            return;
+        }
+
+        closeMenuPanel();
+    });
+}
+
+initialize();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './components/contextMenu/contextMenu';
 export * from './components/datePicker/datePicker';
 export * from './components/dropDown/dropDown';
 export * from './utility/utility';
